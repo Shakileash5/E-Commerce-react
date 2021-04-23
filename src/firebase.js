b7d6735b53474a40cd106359e0f8d0ac35700a4a
shakileash <shakileash2000@gmail.com>
2021-04-23 17:13:15 +0530

    minor update

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import firestore from 'firebase/firestore';
 const settings = {timestampsInSnapshots: true};
 
 const config = {
-  apiKey: "AIzaSyCOQJSl3zPjB2v1KIRh_eubuyKd461mnuU",
+  apiKey: "your - key",
   authDomain: "web-fx-da8ba.firebaseio.com/",
   databaseURL: "https://web-fx-da8ba-default-rtdb.firebaseio.com/",
   projectId: "web-fx-da8ba",
